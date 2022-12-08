2134179996a93c892da4be0e118acbe891411876
박건우 <105593643+PakGeonWoo@users.noreply.github.com>
2022-12-08 17:17:10 +0900

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main_Array(void) {
+
+
+	// 배열 크기는 항상 상수로 선언(변수로 int형 지정해도 안돼)
+	/*int subway_array[3];
+
+	subway_array[0] = 30;
+	subway_array[1] = 40;
+	subway_array[2] = 50;
+
+	for (int i = 0; i <= sizeof(subway_array); i++) {
+		printf("%d\n", subway_array[i]);
+	}*/
+
+	/*int arr[] = { 1, 2 };*/
+
+	return 0;
+
+}

a5af6c3868ba1855934920c3ab686465ee4774ad
박건우 <105593643+PakGeonWoo@users.noreply.github.com>
2022-12-08 17:20:20 +0900

    Add files via upload

---
+++ function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// 선언
+void p(int num); // 함수 정의 전 함수를 선언 해주어야 함.
+
+int main_function() {
+
+	int num = 10;
+	p(num);
+
+}
+
+// 정의
+void p(int num) {
+	printf("num은 %d입니다.\n", num);
+}
